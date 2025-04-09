e55e4ff9967fe85f262fb5bebfb77225b149586e
Alex01921 <learnhtml1921@gmail.com>
2025-04-09 15:38:35 +0530

    Update power_of_number.py

--- power_of_number.py
+++ power_of_number.py
@@ -5,3 +5,13 @@ n = int(input("Enter a number:"))
 for i in range(n+1):
     power = n ** i
 print("Power of number",n,":",power)
+
+# another method
+base = int(input("Enter base no:"))
+exp = int(input("Enter exp no:"))
+result = 1
+
+while exp != 0:
+    result = result * base
+    exp = exp - 1
+print("Power of number",base,":",result)
